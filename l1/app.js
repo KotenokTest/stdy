c78f9ea317e4961265fcb5391bbc59b6cad01145
KotenokTest <63897347+KotenokTest@users.noreply.github.com>
2021-08-17 00:02:53 +0300

    вывакмвапвапапп

--- l1/app.js
+++ l1/app.js
@@ -1,8 +1,22 @@
 var first_text = document.querySelector(".first_text")
-first_text.innerHTML = first_text.textContent.replace(/\S/g, '<span>$&</span>')
-
 var hrf = location.href
 var q = hrf.substring(hrf.indexOf("?")+1)
+var secs = hrf.substring(hrf.indexOf("#")+1)
+
+if (secs.match("//") == null) {
+  if   (q.match("/") == null){
+    text = secs.replace(q, '')
+    text = text.replace('?','')
+    text = text + "&nbsp;"
+    console.log('djfhcidhfskdfk')
+
+  } else {text = secs}
+
+  first_text.innerHTML = first_text.textContent.replace(' ', '').replace(/\S/g, '<span>'+text+'</span>')
+} else {
+  first_text.innerHTML = first_text.textContent.replace(/\S/g, '<span>$&</span>')
+}
+
 
 if (q.length == 3 || q.length == 6) {
   var color = "#"+q
@@ -15,6 +29,5 @@ first_text.addEventListener("mouseover", function(event){
   var et = event.target
   //console.log(event.target)
   et.classList.add('active')
-
   et.style.color = color
 })
