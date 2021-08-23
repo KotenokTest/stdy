a31f93de51f00aa94923f7fa21aaf25bbe98e4c2
KotenokTest <63897347+KotenokTest@users.noreply.github.com>
2021-08-23 18:00:20 +0300

    wqdsfgdfgfgdcxgfxg

---
+++ js-l1/script.js
@@ -0,0 +1,227 @@
+// console.log("ABOBA");
+
+// Htjtbtyyst
+
+// var jfjjffjjfjf = ""
+// const fe2 = 2
+
+// var name = "Sinus"
+// var age = 39
+// var uwu = 'name '+name+" i moi vozrast "+age
+
+// var name = prompt('Как вас зовут')
+// var age = prompt(name+", сколько вам лет?")
+// var all = 'Вас зовут '+name+", вам "+age+" лет"
+// alert(all)
+
+
+
+
+
+// var t1 = 10;
+//     t2 = "20";
+//     t3 = true;
+
+// alert(typeof t1)
+// alert(typeof t2)
+// alert(typeof t3)
+
+
+// a = 10+10
+// b = 10-5
+// c = 10*3
+// d = 10/5
+// e = 10%3
+
+// alert(e)
+
+
+// if (a<939){
+//     console.log("2323232323");
+    
+// } else {
+//     console.log("edddd")
+// }
+
+
+// const IsReady = false
+
+
+// IsReady ? console.log('true'): console.log('false');
+
+
+
+// function calcul(){
+//     console.log(22222)
+// }
+
+// calcul()
+
+
+// console.log(cars[2]);
+
+// console.log(cars[2]);
+
+
+
+
+// // wbrks
+// const cars = ['Beha','Honda', 'Kall', ['123','23455'],0]
+// for (let i = 0; i < cars.length; i++) {
+//     const element = cars[i];
+//     console.log("["+element+"] "+ typeof element);
+    
+    
+// }
+const p = console.log
+// p("")
+// p("")
+
+
+
+
+// const person = {
+//     name: 'ABWSHFA',
+//     age: 15,
+//     lang: ['ru','en','PITON']
+// }
+
+
+// p(person.lang)
+
+
+
+
+
+
+
+// const User = function (name, fam, age, wife){
+//     this.name = name
+//     this.fam = fam
+//     this.age = age
+//     this.wife = wife
+// }
+
+
+
+// let test = new User('Vana', 'Kot', 11, false)
+// let test1 = new User('ira', 'Sobaka', 22, false)
+
+
+// console.log(test, test1);
+
+// name = "ewdsd"
+// console.log(`NAME = ${name}`);
+
+
+
+// let date = new Date()
+
+// p(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`)
+
+// let data = prompt("введите число"); 
+// res = data**2; 
+// alert(res)
+
+const a = alert
+
+
+// ahe = 19
+
+// al(`Мне ${ahe} лет`)
+
+
+// ms = ['h','e','l','l','o']
+// all = ""
+
+// for (let i = 0; i < ms.length; i++) {
+//     const element = ms[i];
+//     all += ms[i]
+
+    
+// }
+
+// p(ms)
+// p(all)
+
+
+
+// ms = [[1,2,3],[4,5,6],[7,8,9]]
+// p(ms[1][0])
+
+
+// a
+
+// pd = {
+//     Alach: 3332,
+//     Boris: 5551,
+//     Nikita: -10
+// }
+
+// a(pd.Boris + "  \n" + pd.Alach)
+
+// while (true){
+// b =prompt()
+// b = Number(b)
+// if (b>0 && b<5){
+//     a("Верно")
+// } else {
+//     a("Неверно")
+// }
+
+
+// }
+
+// b = prompt("Месяц")
+// b = Number(b)
+// m = {
+//     1: 'Зима',
+//     2: 'Зима',
+//     3: 'Весна',
+//     4: 'Весна',
+//     5: 'Весна',
+//     6: 'Лето',
+//     7: 'Лето',
+//     8: 'Лето',
+//     9: 'Осень',
+//     10: 'Осень',
+//     11: 'Осень',
+//     12: 'Зима'
+// }
+// if (b==1 || b==2 || b == 12){
+//     c = 1
+// } else if (b>2 && b<6){
+//     c = 3
+// } else if (b>5 && b<9){
+//     c = 7
+// } else if (b>8 && b<12){
+//     c = 9
+// }
+// a(m[c])
+
+
+
+
+inp = prompt("6 цифер")
+
+
+if (inp.length != 6){
+    a("ПОШЕЛ НАХУЙ")
+} else {
+    l1 = 0
+    l2 = 0
+    for (let i = 0; i < 3; i++) {
+        const element = inp[i];
+        l1+=element
+    }
+    for (let i = 3; i < 6; i++) {
+        const element = inp[i];
+        l2+=element
+    }
+    p(l1, l2)
+    if (l1 == l2){
+        a("Верно")
+    } else {
+        a("Неверно")
+    }
+}
